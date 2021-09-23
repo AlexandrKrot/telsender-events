cb53f37828c58cb9fb2d086a574d9e3fd51e688d
Sasha <alexandrkrot1993@gmail.com>
2021-09-23 20:13:59 +0300

    add esc_attr()

--- assets/template/settings.php
+++ assets/template/settings.php
@@ -10,8 +10,8 @@
 
     <div>
         <label>
-            <input type="text" value="<?php echo $token;?>" name="ts_event_token"
-                   placeholder="<?php echo $default_token;?>" style="width:75%;"/>
+            <input type="text" value="<?php echo esc_attr($token);?>" name="ts_event_token"
+                   placeholder="<?php echo esc_attr($default_token);?>" style="width:75%;"/>
             <span>Telegram token bot</span></label>
     </div>
 
@@ -19,7 +19,7 @@
         <legend>Login Failed</legend>
         <label><input value="1" <?php echo checked( $login_failed );?> name="ts_event_login_failed" type="checkbox"/><span>Enabled</span></label>
         <div>
-            <label><input type="text" value="<?php echo $login_failed_chat_id;?>" name="ts_event_login_failed_chat_id" placeholder="<?php echo $default_chat_id;?>"/><span> Send Chat id</span></label>
+            <label><input type="text" value="<?php echo esc_attr($login_failed_chat_id);?>" name="ts_event_login_failed_chat_id" placeholder="<?php echo esc_attr($default_chat_id);?>"/><span> Send Chat id</span></label>
         </div>
     </fieldset>
 
@@ -27,7 +27,7 @@
         <legend>Login success</legend>
         <label><input value="1" <?php echo checked( $login_success );?> name="ts_event_login_success" type="checkbox"/><span>Enabled</span></label>
         <div>
-            <label><input type="text" value="" name="ts_event_login_success_chat_id" placeholder="<?php echo $login_success_chat_id;?>"/><span> Send Chat id</span></label>
+            <label><input type="text" value="" name="ts_event_login_success_chat_id" placeholder="<?php echo esc_attr($login_success_chat_id);?>"/><span> Send Chat id</span></label>
         </div>
     </fieldset>
 
@@ -38,7 +38,7 @@
             <div>
                 <label>
                     <input type="text" value="<?php echo $interception_post_chat_id;?>" name="ts_event_interception_post_chat_id"
-                           placeholder="<?php echo $default_chat_id;?>"/><span> Send Chat id</span>
+                           placeholder="<?php echo esc_attr($default_chat_id);?>"/><span> Send Chat id</span>
                 </label>
             </div>
 
@@ -68,15 +68,15 @@
 
         <label><input value="1" <?php echo checked( $utm );?> name="ts_event_utm" type="checkbox"/><span>Enabled</span></label>
         <div>
-            <label><input type="text" value="<?php echo $utm_chat_id?>" name="ts_event_utm_chat_id"
-                          placeholder="<?php echo $default_chat_id;?>"/><span> Send Chat id</span></label>
+            <label><input type="text" value="<?php echo esc_attr($utm_chat_id);?>" name="ts_event_utm_chat_id"
+                          placeholder="<?php echo esc_attr($default_chat_id);?>"/><span> Send Chat id</span></label>
         </div>
         <div class="utm_list_wrap">
             <?php if (is_array($utm_list_val)): ?>
                 <?php  foreach ($utm_list_val as $key => $utm) : ?>
                     <div class="utm_list">
-                        <input type="text" value="<?php echo  $utm['source']?>" name="ts_event_utm_list_param[]" placeholder="utm params"/><input
-                            type="text" value="<?php echo $utm['value']?>" name="ts_event_utm_list_value[]" placeholder="utm params value"/>
+                        <input type="text" value="<?php echo  esc_attr($utm['source'])?>" name="ts_event_utm_list_param[]" placeholder="utm params"/><input
+                            type="text" value="<?php echo  esc_attr($utm['value']);?>" name="ts_event_utm_list_value[]" placeholder="utm params value"/>
                         <button type="button" class="add">+</button>
                         <button type="button" class="remove">&times</button>
                     </div>
@@ -91,7 +91,7 @@
         <legend>Woocommerce Add To Cart</legend>
         <label><input value="1" <?php echo checked( $wc_add_to_cart );?> checked="" name="ts_event_wc_add_to_cart" type="checkbox"/><span>Enabled</span></label>
         <div>
-            <label><input type="text" value="<?php echo $wc_add_to_cart_chat_id;?>" name="ts_event_wc_add_to_cart_chat_id" placeholder="<?php echo $default_chat_id?>"/><span> Send Chat id</span></label>
+            <label><input type="text" value="<?php echo  esc_attr($wc_add_to_cart_chat_id);?>" name="ts_event_wc_add_to_cart_chat_id" placeholder="<?php echo esc_attr($default_chat_id)?>"/><span> Send Chat id</span></label>
         </div>
     </fieldset>
 
@@ -103,7 +103,7 @@
             <div class="bots_list"><select multiple="multiple" name="ts_event_bot_list_value[]"
                                            placeholder="Bot params">
                     <?php foreach ($searchBots as $botValue =>$botName): ?>
-                        <option <?php echo  selected($bots_list_val,$botValue); ?> value="<?php echo $botValue;?>"> <?php echo $botName;?> </option>
+                        <option <?php echo  selected($bots_list_val,$botValue); ?> value="<?php echo  esc_attr($botValue);?>"> <?php echo $botName;?> </option>
                     <?php endforeach; ?>
                     <option selected value="">Other</option>
                 </select>
